ca8f2c9498c07a60e585bdf8ae6f97cba9271e28
podol0207 <audrhks0207@naver.com>
2024-02-26 15:32:55 +0900

    [FEAT]-Login

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import image from "../users/image/카카오.png";
+
 export default function Login() {
   return (
-    <div className="bg-white text-black flex min-h-screen flex-col items-center sm:justify-center sm:pt-0">
+    <div className="bg-white text-black flex flex-col items-center min-h-screen sm:justify-center sm:pt-0">
       <div className="relative mt-12 w-full max-w-lg sm:mt-10">
         <div className="divide-y divide-gray-300 mx-5 border rounded-lg text-center lg:rounded-xl">
           <div className="p-2 text-base font-semibold">
@@ -12,13 +15,13 @@ export default function Login() {
               에어비앤비에 오신 것을 환영합니다.
             </h3>
             <form>
-              <div className="relative rounded-lg border-2 border-gray-200 px-2 pb-2.5 pt-2.5 my-3.5 duration-200 focus-within:ring focus-within:ring-black">
+              <div className="relative rounded-lg border-2 border-gray-200 px-2 pb-2.5 pt-2.5 my-3.5 focus-within:ring focus-within:ring-black">
                 <input
                   type="text"
                   name="이메일"
                   placeholder="이메일"
                   autocomplete="off"
-                  className="block w-full border-1 bg-transparent p-0 text-sm file:my-1 file:rounded-full file:border-0 file:bg-accent file:px-4 file:py-2 file:font-medium placeholder:text-muted-foreground/90 focus:outline-none focus:ring-0 sm:leading-7 text-foreground"
+                  className="block w-full bg-transparent p-0 text-sm file:my-1 file:rounded-full file:border-0 file:bg-accent file:px-4 file:py-2 file:font-medium placeholder:text-muted-foreground/90 focus:outline-none focus:ring-0 sm:leading-7 text-foreground"
                 />
               </div>
               <button
@@ -28,11 +31,17 @@ export default function Login() {
                 계속
               </button>
             </form>
-            <h3>-------또는-------</h3>
+            <div class="inline-flex items-center justify-center w-full">
+              <hr className="w-full h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+              <span className="absolute px-3 font-medium text-gray-900 bg-white ">
+                또는
+              </span>
+            </div>
             <button
-              className="mt-4 font-semibold w-full border border-black rounded-md text-sm bg-white text-black h-10"
+              className="flex font-semibold w-full border border-black rounded-md text-sm bg-white text-black h-10"
               type="submit"
             >
+              <Image src={image} alt="" className="size-4" />
               카카오로 로그인하기
             </button>
           </div>
