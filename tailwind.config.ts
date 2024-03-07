e30779765342e0c8e61972d85856ebc6faf9e722
honi <winner020531@naver.com>
2024-03-07 16:58:25 +0900

    [ERROR]-.env 삭제

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,20 +27,6 @@ const config: Config = {
         slideUp: 'slideUp 0.4s ease-out',
         fadeIn: 'fadeIn 0.4s ease-in-out',
       },
-      keyframes: {
-        slideUp: {
-          "0%": { transform: "translateY(100%)" },
-          "100%": { transform: "translateY(0)" },
-        },
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
-        },
-      },
-      animation: {
-        slideUp: "slideUp 0.4s ease-out",
-        fadeIn: "fadeIn 0.1s ease-in-out",
-      },
     },
   },
   plugins: [],
