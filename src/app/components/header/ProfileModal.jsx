5057175d3fcca07c09dd5a88e27050e16202b532
honi <winner020531@naver.com>
2024-03-22 13:18:58 +0900

    [FEAT]-프로필 모달과 로그인 연결

--- src/app/components/header/ProfileModal.jsx
+++ src/app/components/header/ProfileModal.jsx
@@ -1,39 +1,52 @@
+'use client'
+import React, { useState } from 'react'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
-
+import LoginModal from '@/app/components/loginmodal'
 export default function ProfileModal({ anchorEl, setAnchorEl }) {
   const isOpen = Boolean(anchorEl)
   const onClose = () => {
     setAnchorEl(null)
   }
-
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
+  const handleLoginClick = () => {
+    setIsLoginModalOpen(true)
+    onClose() // 모달 열리면 메뉴 닫기
+  }
   return (
-    <Menu
-      id="profil-modal"
-      anchorEl={anchorEl}
-      keepMounted
-      open={isOpen}
-      onClose={onClose}
-      sx={{
-        '.MuiPaper-root': {
-          borderRadius: '16px',
-          marginTop: '12px',
-          marginRight: '24px',
-        },
-      }}
-    >
-      <MenuItem sx={{ fontSize: '14px', fontWeight: 'bold', padding: '16px' }}>
-        로그인
-      </MenuItem>
-      <MenuItem sx={{ fontSize: '14px', padding: '16px' }}>회원가입</MenuItem>
-      <hr />
-      <MenuItem sx={{ fontSize: '14px', padding: '16px' }}>
-        당신의 공간을 에어비앤비하세요
-      </MenuItem>
-      <MenuItem sx={{ fontSize: '14px', padding: '16px' }}>
-        도움말 센터
-      </MenuItem>
-    </Menu>
-    // {isModalOpen && <LoginModal setIsModalOpen={setIsModalOpen} />}
+    <>
+      <Menu
+        id="profil-modal"
+        anchorEl={anchorEl}
+        keepMounted
+        open={isOpen}
+        onClose={onClose}
+        sx={{
+          '.MuiPaper-root': {
+            borderRadius: '16px',
+            marginTop: '12px',
+            marginRight: '24px',
+          },
+        }}
+      >
+        <MenuItem
+          sx={{ fontSize: '14px', fontWeight: 'bold', padding: '16px' }}
+        >
+          <button onClick={handleLoginClick}>로그인</button>
+        </MenuItem>
+        <MenuItem sx={{ fontSize: '14px', padding: '16px' }}>회원가입</MenuItem>
+        <hr />
+        <MenuItem sx={{ fontSize: '14px', padding: '16px' }}>
+          당신의 공간을 에어비앤비하세요
+        </MenuItem>
+        <MenuItem sx={{ fontSize: '14px', padding: '16px' }}>
+          도움말 센터
+        </MenuItem>
+      </Menu>
+      <LoginModal
+        openModal={isLoginModalOpen}
+        onClose={() => setIsLoginModalOpen(false)}
+      />
+    </>
   )
 }
