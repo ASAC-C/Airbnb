37c97a6dfd22945c706ad330aeec58dacb5f312a
honi <winner020531@naver.com>
2024-03-22 14:13:35 +0900

    [FEAT]-api 수정

--- src/app/api/home/route.js
+++ src/app/api/home/route.js
@@ -1,28 +1,17 @@
 import RoomsData from '@/app/lib/data.json'
 export async function GET() {
   const roomsList = RoomsData.data.rooms
-  const set = roomsList.map((room) => {
-    const id = parseInt(room.id)
-    const image = room.images
-    const country = room.country
-    const location = room.location
-    const price = room.price
-    const distance = room.distance
-    const date = room.date
-    const description = room.description
-    const rating = room.rating
-    return {
-      id,
-      image,
-      country,
-      location,
-      price,
-      distance,
-      date,
-      description,
-      rating,
-    }
-  })
+  const set = roomsList.map((room) => ({
+    id: parseInt(room.id),
+    image: room.images,
+    country: room.country,
+    location: room.location,
+    price: room.price,
+    distance: room.distance,
+    date: room.date,
+    description: room.description,
+    rating: room.rating,
+  }))
 
   return Response.json({ data: JSON.stringify(set) })
 }
